12879dad8001150b80e52ef604a60feb6e5cfadb
janniofsantos <jannioferreira@gmail.com>
2026-09-12 13:10:15 -0300

    port 8000 host 0.0.0.0

--- app.py
+++ app.py
@@ -56,5 +56,7 @@ def index():
     )
 
 
-if __name__ == "__main__":
-    app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+if __name__ == '__main__':
+    host = os.environ.get('HOST', '0.0.0.0')
+    port = int(os.environ.get('PORT', '8000'))
+    app.run(host=host, port=port, debug=False)
